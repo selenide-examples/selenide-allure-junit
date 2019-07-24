efd248dcedd2952ef48e3f64edee44c7535e2056
Andrei Solntsev <andrei.solntsev@gmail.com>
2019-07-24 23:10:23 +0300

    update project to latest Selenide and Allure

--- src/test/java/org/selenide/examples/GoogleSearchTest.java
+++ src/test/java/org/selenide/examples/GoogleSearchTest.java
@@ -3,16 +3,18 @@ package org.selenide.examples;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleSearchTest {
   @Test
   public void userCanSearchAnyKeyword() {
-    open("http://google.com/en");
+    open("https://google.com/ncr");
     $(By.name("q")).val("selenide").pressEnter();
-    $$("#ires .g").shouldHave(size(10));
-    $("#ires .g").shouldHave(text("selenide.org"));
+    $$("#res .g").shouldHave(sizeGreaterThan(5));
+    $("#res .g").shouldHave(text("selenide.org"));
   }
 }
--- src/test/java/org/selenide/examples/InternetBankTest.java
+++ src/test/java/org/selenide/examples/InternetBankTest.java
@@ -1,10 +1,12 @@
 package org.selenide.examples;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Screenshots;
 import com.codeborne.selenide.SelenideElement;
 import com.google.common.io.Files;
-import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import ru.yandex.qatools.allure.annotations.Attachment;
@@ -13,68 +15,88 @@ import java.io.File;
 import java.io.IOException;
 
 import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.or;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.FileDownloadMode.PROXY;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.open;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.commons.io.FileUtils.readFileToString;
 import static org.junit.Assert.assertTrue;
 
 public class InternetBankTest {
-  @Test
-  public void userCanLoginToBspbDemo() {
-    open("http://idemo.bspb.ru/");
+  @BeforeClass
+  public static void setUp() {
+    close();
+    Configuration.baseUrl = "https://idemo.bspb.ru";
+    Configuration.proxyEnabled = true;
+    Configuration.fileDownload = PROXY;
+  }
+
+  @Before
+  public void login() {
+    open("/");
     $(By.name("username")).val("demo");
     $(By.name("password")).val("demo").pressEnter();
-    $(By.name("otpCode")).val("0000").pressEnter();
+    enter2ndFactor();
+  }
 
-    $("#user-greeting").shouldHave(text("Ольга Королёва"));
-    $(".page-header").shouldHave(text("Overview"));
+  private void enter2ndFactor() {
+    $("#otp-code-text").shouldBe(visible);
+    if ($("#login-crypto-button").isDisplayed()) {
+      $("#login-crypto-button").click();
+    }
+    else {
+      $(By.name("otpCode")).val("0000").pressEnter();
+    }
+  }
+
+  @Test
+  public void userCanLoginToBspbDemo() {
+    $("#user-greeting").shouldHave(text("Hello World!"));
   }
 
   @Test
   public void userCanLoadStatementForLastMonth() {
-    open("http://idemo.bspb.ru/security/fakeLogin?username=demo&url=/bank/overview");
+    open("/bank/overview");
 
-    $("#account-2 a").click();
+    $("#accounts .account a").click();
     $(".page-header").shouldHave(text("Statement"));
     $("#defined-periods").find(byText("Beginning of last month until today")).click();
-
-//    random data, cannot test regularly :(
-//    $$("#statement-2 .transaction-row").shouldHave(size(71));
-//    $("#statement-2 .transaction-row").shouldHave(
-//        text("Лукойл"),
-//        text("83151810351541100000"),
-//        text("Заправка бензина 95Е, 40 л."),
-//        text("+3.00"),
-//        text("-256.00")
-//    );
   }
 
   @Test
   public void userCanViewTransactionDetails() {
     openStatement();
-    $("#statement-2 .transaction-row a").click();
+    $(".statement-container .transaction-row.tx-debit .counterparty-name").scrollTo().click();
     $("#transaction-dialog #transaction-header").shouldHave(text("Transaction details"));
     $("#transaction-dialog #payment-beneficiary").shouldHave(text("Beneficiary"));
-    $("#transaction-dialog #payment-beneficiary-account").shouldHave(
-        text("Beneficiary account"), text("83151 810 3 5154 1100000"));
+    $("#transaction-dialog #beneficiary-account").shouldHave(
+        text("Beneficiary account"),
+        text("***** *** * **** ***0000")
+    );
   }
 
   @Test
   public void userCanDownloadStatementAsPdf() throws IOException {
     openStatement();
+    $("#statement-export").click();
     File statementPdf = $("#btn-export-pdf").download();
-    assertTrue(FileUtils.readFileToString(statementPdf).startsWith("%PDF"));
-    // verify PDF content
+    assertTrue(readFileToString(statementPdf, UTF_8).startsWith("%PDF"));
+    // TODO verify PDF content
   }
 
   @Test
   public void userCanAssignAliasForAccount() {
-    open("http://idemo.bspb.ru/security/fakeLogin?username=demo&url=/bank/overview");
-    SelenideElement account = $(".account#account-1");
-    
+    open("/bank/overview");
+    SelenideElement account = $("#accounts .account", 2).scrollTo();
+
     account.find("a.alias")
-        .shouldHave(text("40817 810 0 4800 0102279"))
+        .shouldHave(or("account or alias", text("50817 810 0 4800 0104467"), text("Это типа счёт"))
+            .because("the alias could already be changed by previous test run"))
         .hover();
     
     account.find(".icon-edit").should(appear).click();
@@ -82,11 +104,11 @@ public class InternetBankTest {
     
     account.shouldHave(text("Это типа счёт"));
     
-    account.shouldNotHave(text("40817 810 0 4800 0102279"));
+    account.shouldNotHave(text("50817 810 0 4800 0104467"));
   }
 
   private void openStatement() {
-    open("http://idemo.bspb.ru/security/fakeLogin?username=demo&url=/statement");
+    open("/statement");
     $("#defined-periods").find(byText("Beginning of last month until today")).click();
   }
 
@@ -98,6 +120,6 @@ public class InternetBankTest {
   @Attachment(type = "image/png")
   public byte[] screenshot() throws IOException {
     File screenshot = Screenshots.getLastScreenshot();
-    return Files.toByteArray(screenshot);
+    return screenshot == null ? null : Files.toByteArray(screenshot);
   }
 }
