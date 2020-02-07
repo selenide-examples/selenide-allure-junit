7d43ee24baba259a6a5fd9e13e683afa12e6e9a0
gabrielstar <gabriel.starczewski@gmail.com>
2020-02-07 11:49:17 +0100

    added logger

--- src/main/java/tests/BaseTests.java
+++ src/main/java/tests/BaseTests.java
@@ -4,15 +4,18 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.junit.ScreenShooter;
 import com.google.common.collect.ImmutableMap;
 import configuration.EnvironmentPropertiesLoader;
-import io.qameta.allure.Attachment;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Rule;
 import org.junit.rules.TestName;
+import ru.yandex.qatools.allure.annotations.Attachment;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static com.codeborne.selenide.Selenide.screenshot;
 import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
@@ -25,6 +28,7 @@ public class BaseTests {
     public ScreenShooter makeScreenshotOnFailure = ScreenShooter.failedTests().succeededTests();
     @Rule
     public TestName name = new TestName();
+    private static Logger log = Logger.getLogger("BaseTests");
 
     static {
         setAllureEnvironment();
@@ -42,20 +46,33 @@ public class BaseTests {
     private static void setSelenideConfiguration() {
         Configuration.screenshots = true;
         Configuration.savePageSource = false;
-        Configuration.reportsFolder = "target/allure-results"; //write screenshot to allure
+        Configuration.reportsFolder = "target/allure-results"; //write screenshot to allure with prettyName
     }
 
 
     public byte[] saveScreenshotOnDiskAndReturn() {
-        screenshot(name.getMethodName());
-        Path content = Paths.get("target" + System.getProperty("file.separator") + "allure-results", name.getMethodName() + ".png").toAbsolutePath();
         try {
+            screenshot(name.getMethodName());
+            Path content = Paths.get("target" + System.getProperty("file.separator") + "allure-results", name.getMethodName() + ".png").toAbsolutePath();
+            log.log(Level.INFO, "Screenshot being done for " + name.getMethodName());
             return Files.readAllBytes(content);
+
         } catch (IOException e) {
-            return null;
+            log.log(Level.INFO, e.getStackTrace().toString());
         }
+        return  null;
     }
 
+    @After
+    public void tearDown() throws IOException {
+        attachScreenshot();
+    }
+
+    @Attachment(type = "image/png")
+    public byte[] attachScreenshot() throws IOException {
+
+        return saveScreenshotOnDiskAndReturn();
+    }
 
     @AfterClass
     public static void closeBrowser() {
--- src/test/java/org/selenide/examples/pages/GoogleSearchTest.java
+++ src/test/java/org/selenide/examples/pages/GoogleSearchTest.java
@@ -1,33 +1,16 @@
 package org.selenide.examples.pages;
 
-import org.junit.After;
 import org.junit.Test;
 import pages.app.GoogleResultsPage;
 import pages.app.GoogleSearchPage;
-import ru.yandex.qatools.allure.annotations.Attachment;
 import tests.BaseTests;
-
-import java.io.IOException;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleSearchTest extends BaseTests {
-
-
     @Test
     public void userCanSearchAnyKeywordAndResultsPageIsReturned() {
         GoogleSearchPage googleSearchPage = open(GoogleSearchPage.getAbsoluteUrl(), GoogleSearchPage.class);
         GoogleResultsPage googleResultsPage = googleSearchPage.search("dupa");
-    }
-
-    @After
-    public void tearDown() throws IOException {
-        screenshot();
-    }
-
-    @Attachment(type = "image/png")
-    public byte[] screenshot() throws IOException {
-
-        return saveScreenshotOnDiskAndReturn();
     }
 }
