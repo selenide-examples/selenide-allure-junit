6aa3007f76ee83a504ac249e537ad84567333edd
gabrielstar <gabriel.starczewski@gmail.com>
2020-02-07 11:16:12 +0100

    Working allure attachements

--- src/main/java/tests/BaseTests.java
+++ src/main/java/tests/BaseTests.java
@@ -1,18 +1,34 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
+import com.codeborne.selenide.junit.ScreenShooter;
 import com.google.common.collect.ImmutableMap;
 import configuration.EnvironmentPropertiesLoader;
+import io.qameta.allure.Attachment;
+import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static com.codeborne.selenide.Selenide.screenshot;
 import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 
 /**
  * Bootstrap class as BaseTests should be run before anything else once
  */
 public class BaseTests {
+    @Rule
+    public ScreenShooter makeScreenshotOnFailure = ScreenShooter.failedTests().succeededTests();
+    @Rule
+    public TestName name = new TestName();
+
     static {
         setAllureEnvironment();
+        setSelenideConfiguration();
     }
 
     private static void setAllureEnvironment() {
@@ -21,5 +37,28 @@ public class BaseTests {
                         .put("Browser", Configuration.browser)
                         .put("URL", EnvironmentPropertiesLoader.getProps().getProperty("BASE_URL"))
                         .build());
+    }
+
+    private static void setSelenideConfiguration() {
+        Configuration.screenshots = true;
+        Configuration.savePageSource = false;
+        Configuration.reportsFolder = "target/allure-results"; //write screenshot to allure
+    }
+
+
+    public byte[] saveScreenshotOnDiskAndReturn() {
+        screenshot(name.getMethodName());
+        Path content = Paths.get("target" + System.getProperty("file.separator") + "allure-results", name.getMethodName() + ".png").toAbsolutePath();
+        try {
+            return Files.readAllBytes(content);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+
+    @AfterClass
+    public static void closeBrowser() {
+        //close();
     }
 }
--- src/test/java/org/selenide/examples/pages/GoogleSearchTest.java
+++ src/test/java/org/selenide/examples/pages/GoogleSearchTest.java
@@ -1,7 +1,5 @@
 package org.selenide.examples.pages;
 
-import com.codeborne.selenide.Screenshots;
-import com.google.common.io.Files;
 import org.junit.After;
 import org.junit.Test;
 import pages.app.GoogleResultsPage;
@@ -9,18 +7,27 @@ import pages.app.GoogleSearchPage;
 import ru.yandex.qatools.allure.annotations.Attachment;
 import tests.BaseTests;
 
-import java.io.File;
 import java.io.IOException;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleSearchTest extends BaseTests {
 
+
     @Test
     public void userCanSearchAnyKeywordAndResultsPageIsReturned() {
         GoogleSearchPage googleSearchPage = open(GoogleSearchPage.getAbsoluteUrl(), GoogleSearchPage.class);
         GoogleResultsPage googleResultsPage = googleSearchPage.search("dupa");
+    }
 
-        System.out.println("AA: " + googleResultsPage.getAbsoluteUrl());
+    @After
+    public void tearDown() throws IOException {
+        screenshot();
+    }
+
+    @Attachment(type = "image/png")
+    public byte[] screenshot() throws IOException {
+
+        return saveScreenshotOnDiskAndReturn();
     }
 }
