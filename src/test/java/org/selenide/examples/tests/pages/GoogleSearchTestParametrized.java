5066da8685ce3b05273e282dcdbe9042cd8cf874
gabrielstar <gabriel.starczewski@gmail.com>
2020-02-11 16:01:34 +0100

    Added parametrized search case

--- src/test/java/org/selenide/examples/tests/pages/GoogleSearchTestParametrized.java
+++ src/test/java/org/selenide/examples/tests/pages/GoogleSearchTestParametrized.java
@@ -1,6 +1,10 @@
 package org.selenide.examples.tests.pages;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.selenide.examples.config.pages.app.GoogleResultsPage;
 import org.selenide.examples.config.pages.app.GoogleSearchPage;
 import org.selenide.examples.config.tests.BaseTests;
@@ -8,9 +12,18 @@ import ru.yandex.qatools.allure.annotations.Features;
 import ru.yandex.qatools.allure.annotations.Issue;
 import ru.yandex.qatools.allure.annotations.Stories;
 
-public class GoogleSearchTest extends BaseTests {
+@RunWith(Parameterized.class)
+public class GoogleSearchTestParametrized extends BaseTests {
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
     GoogleResultsPage googleResultsPage = new GoogleResultsPage();
+
+    @Parameters
+    public static Object[] data() {
+        return new Object[]{"Common phrase 1", "Common phrase 2", "Common phrase3"};
+    }
+
+    @Parameter
+    public String commomnSearchPhrase;
 
     @Test
     @Features("Searches for common phrases")
@@ -18,7 +31,7 @@ public class GoogleSearchTest extends BaseTests {
     @Issue("JIRA-67")
     public void userCanSearchAnyKeywordAndResultsPageIsReturned() {
         googleSearchPage.open();
-        googleResultsPage = googleSearchPage.search("common phrase");
+        googleResultsPage = googleSearchPage.search(commomnSearchPhrase);
         assert true;
     }
 }
--- src/test/java/org/selenide/examples/tests/suites/RegressionTestsSuite.java
+++ src/test/java/org/selenide/examples/tests/suites/RegressionTestsSuite.java
@@ -3,14 +3,13 @@ package org.selenide.examples.tests.suites;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.selenide.examples.tests.pages.GoogleSearchAssertFailsTest;
-import org.selenide.examples.tests.pages.GoogleSearchTest;
-import org.selenide.examples.tests.pages.GoogleSearchThrowsExceptionTest;
+import org.selenide.examples.tests.pages.GoogleSearchTestParametrized;
 import org.selenide.examples.tests.pages.PagesTest;
 
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
-        GoogleSearchTest.class,
+        GoogleSearchTestParametrized.class,
         GoogleSearchAssertFailsTest.class,
         PagesTest.class,
 
