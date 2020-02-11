b9c91c5aea7ce2933d999cbab045e3d39f5caf9a
gabrielstar <gabriel.starczewski@gmail.com>
2020-02-11 15:40:21 +0100

    selenide assert added

--- src/test/java/org/selenide/examples/tests/pages/GoogleSearchAssertFailsTest.java
+++ src/test/java/org/selenide/examples/tests/pages/GoogleSearchAssertFailsTest.java
@@ -5,14 +5,17 @@ import org.selenide.examples.config.pages.app.GoogleResultsPage;
 import org.selenide.examples.config.pages.app.GoogleSearchPage;
 import org.selenide.examples.config.tests.BaseTests;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+
 public class GoogleSearchAssertFailsTest extends BaseTests {
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
     GoogleResultsPage googleResultsPage = new GoogleResultsPage();
 
     @Test
     public void userCanSearchAnyKeywordThenResultsPageIsReturnedThenAssertFails() {
+        final int EXPECTED_MINIMUM_SIZE = 9999;
         googleSearchPage.open();
         googleResultsPage = googleSearchPage.search("Uncommon phrase");
-        googleResultsPage.getResults().shouldHaveSize(9999);
+        googleResultsPage.getResults().shouldHave(sizeGreaterThan(EXPECTED_MINIMUM_SIZE));
     }
 }
