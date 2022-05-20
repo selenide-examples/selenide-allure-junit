c494076af0e011d87d9abb5fc6d295c43b6907e4
oussamabadr <oussamabadr@gmail.com>
2022-05-21 01:54:26 +0200

    Upgrade to Junit 5

--- src/test/java/org/selenide/examples/DuckSearchTest.java
+++ src/test/java/org/selenide/examples/DuckSearchTest.java
@@ -1,11 +1,11 @@
 package org.selenide.examples;
 
-import com.codeborne.selenide.junit.TextReport;
+import com.codeborne.selenide.junit5.TextReportExtension;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.CollectionCondition.size;
@@ -15,17 +15,16 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class DuckSearchTest {
-  @Rule
-  public TextReport report = new TextReport();
+@ExtendWith(TextReportExtension.class)
+class DuckSearchTest {
 
-  @Before
+  @BeforeEach
   public void setUp() {
     SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
   }
 
   @Test
-  public void userCanSearchAnyKeyword() {
+  void userCanSearchAnyKeyword() {
     open("https://duckduckgo.com/");
     $(By.name("q")).val("selenide").pressEnter();
     $$(".js-results").shouldHave(size(1));
